78f6a8c9b0118978feda33e0baa74065b3d0e0ff
unknown <sebastian.agudelo0531@gmail.com>
2024-03-07 16:31:09 -0500

    subo la parte donde se le administra la creacion de la tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import {useRef ,useState } from 'react'
-
+import { Task } from '../Task/Task'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,28 +6,12 @@ import { LastTask } from './components/LastTask/LastTask'
 import { Task } from './components/Task/Task'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const myReference= useRef('')
-  const Task1=[
-    {
-    _id:1,
-    description:"cualquier cosa",
-  },
- 
-]
+  
+  
+  
   return (
    <>
   <LastTask/>
-   <div className='array'>
-      {
-        Task1.map(TAsks=>(
-          <Task
-          key={TAsks._id}
-          content={TAsks.description}
-          />
-        ))
-      }
-   </div>
   
    </>
   )
--- src/components/LastTask/LastTask.jsx
+++ src/components/LastTask/LastTask.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import {useRef ,useState } from 'react'
+
+
 
 export const LastTask = () => {
+  const myReference = useRef('')
+  const [tasks, setTasks] = useState ([])
+
+  const CreationTaks=()=>{
+    let response=myReference.current.value
+    console.log(response);
+    counter++
+    const NewTaks={
+      _id: counter,
+      takss: response
+    }
+  
+     setTakks([...tasks, NewTaks])
+  }
+
   return (
 <>
     
    <div className='notes'>
    <input ref={myReference} type="text" placeholder='ingresa una tarea' />
-   <button >crear tarea</button>
+   <button onClick={CreationTaks}>crear tarea</button>
    </div>
+   <div className="ContentTaks">
+        {
+            tasks.map(TAks=>(
+            <Taks 
+            key={TAks._id}
+            component={TAks.takss}/>
+          ))
+        }
+      </div>
    </>
 
   )
